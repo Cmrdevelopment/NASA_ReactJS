951338d4862afa4de6f4b4b9c97ea3604e5fc448
Cmrdevelopment <cmrbolsadevelopment@gmail.com>
2023-06-03 23:10:04 +0100

    actualización

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
+// importamos los archivos necesario para que nos funcione nuestra web
 import { useState, useEffect } from "react";
 import Figure from "./components/figure/Figure";
 import axios from "axios";
 import "./App.css";
 
 const App = () => {
+  // Obtenemos la fecha y la guardamos en today, recibimos los datos con los 10 caracteres de la fecha "AAAA-MM-DD"
   const today = new Date(Date.now()).toISOString().slice(0, 10);
+  // useState crea un valor (estado) cuyo valor inicial es apod y lo cambia setApod
   const [apod, setApod] = useState({});
+  // useState crea un valor (estado) cuyo valor inicial es date y lo cambia setDate
   const [date, setDate] = useState(today);
   const NASA_URL = "https://api.nasa.gov/";
   const NASA_API_KEY = "hwxeHOhb2uHTbyzru7vzevCbon5VB3dVyjqYACZP";
+  // Función que representa el evento de entrada almacenando el día que queremos observar
   const handleInput = (ev) => {
     setDate(ev.target.value.toLocaleString());
   };
-
+  // useEffect lo utilizamos para llamar a la api y al ser extena, la api, debemos utilizar el useEffect
   useEffect(() => {
     const getApod = async () => {
       const data = await axios.get(
@@ -26,24 +31,29 @@ const App = () => {
   return (
     <div className="App">
       <h2 className="title">
-        NASA API{" "}
+        NASA API {/* logo y titulo de la página (nasa) */}
         <img
           src="https://res.cloudinary.com/dwdznuzaz/image/upload/v1685725002/NASA_logo.svg_c16krg.png"
           className="logo"
           alt="NASA LOGO"
         />
       </h2>
-      <h1>Astronomy Picture of the Day</h1>
+      {/* Pintamos titulo y el input de entrada de la fecha a elegir y cuando se cambie el día se ejecuta la función handleInput   */}
+      <h1>Foto astronomia del dia seleccionado</h1>
       <input type="date" id="photo-date" onChange={handleInput} />
       {date > today ? (
         <h2>Please choose a previous date</h2>
       ) : (
+        // Se ejecuta la función figure para que se rendenderice la foto del día elegido
         <Figure data={apod} />
       )}
+      {/* pie de página con la url de la api.nasa */}
       <div className="standard-dialog center">
         <h1 className="dialog-text">
           @cmrbolsa - 2023 -{" "}
-          <a href="https://api.nasa.gov/">https://api.nasa.gov/</a>
+          <a href="https://api.nasa.gov/" Target="_blank">
+            https://api.nasa.gov/
+          </a>
         </h1>
       </div>
     </div>
--- src/components/figure/Figure.jsx
+++ src/components/figure/Figure.jsx
@@ -3,17 +3,21 @@ import "./Figure.css";
 const Figure = ({ data }) => {
   return (
     <figure>
+      {/* url de la imagen en el api.nasa para que al buscar se represente */}
       <img src={data.url} alt={data.title} />
       <div className="window">
         <div className="title-bar">
           <button aria-label="Close" className="close"></button>
+          {/* titulo que sale debajo de la foto */}
           <h1 className="title">{data.title}</h1>
           <button aria-label="Resize" className="resize"></button>
         </div>
         <div className="details-bar">
+          {/* Dia y copyright de la foto antes de la descripción */}
           <span>{data.date}</span>
           <span>{data.copyright}</span>
         </div>
+        {/* descripción de la foto del día elegido */}
         <div className="window-pane">{data.explanation}</div>
       </div>
     </figure>
